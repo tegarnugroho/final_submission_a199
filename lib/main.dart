7539d6381756988eb43f3fc95b11e7c0a581babd
Tegar Nugroho <tegaranugroho@gmail.com>
2022-04-01 09:07:40 +0700

    Change MultiProvider with MultiBlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import 'package:ditonton/injection.dart' as di;
 
 
@@ -47,7 +46,7 @@ void main() async{
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
+    return MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => di.locator<NowPlayingTvSeriesBloc>()),
         BlocProvider(create: (_) => di.locator<PopularTvSeriesBloc>()),
